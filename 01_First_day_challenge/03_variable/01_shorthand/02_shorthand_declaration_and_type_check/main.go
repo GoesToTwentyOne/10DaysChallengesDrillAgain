69c71c419def4824f587cfb71669db6e213443d1
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-05-05 05:16:24 +0600

    shorthand declation and type checking

---
+++ 01_First_day_challenge/03_variable/01_shorthand/02_shorthand_declaration_and_type_check/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := "Nihad Hossain"
+	b := 'A'
+	c := 40
+	d := 4.97
+	e := true
+	f := `Nihad Hossain 2`
+	fmt.Printf("%T   %v \n", a, a)
+	fmt.Printf("%T   %v \n", b, b)
+	fmt.Printf("%T   %v \n", c, c)
+	fmt.Printf("%T   %v \n", d, d)
+	fmt.Printf("%T   %v \n", e, e)
+	fmt.Printf("%T   %v \n", f, f)
+
+}
