3bb9cecad1fbab9064e12199cb6e9b2cd1d52ca6
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-05-05 05:12:53 +0600

    shorthand declaration

---
+++ 01_First_day_challenge/03_variable/01_shorthand/01_shorthand_declaretion/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := "Nihad Hossain"
+	b := 'A'
+	c := 40
+	d := 4.97
+	e := true
+	f := `Nihad Hossain 2`
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+	fmt.Printf("%v \n", e)
+	fmt.Printf("%v \n", f)
+
+}
