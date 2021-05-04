75fbb66bf0984cca20e9418bbfbe84504e4d233a
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-05-05 05:19:35 +0600

    zero values

---
+++ 01_First_day_challenge/03_variable/02_var_zero_value/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string
+	var b rune
+	var c int
+	var d bool
+	var e float64
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+	fmt.Printf("%v \n", e)
+}
