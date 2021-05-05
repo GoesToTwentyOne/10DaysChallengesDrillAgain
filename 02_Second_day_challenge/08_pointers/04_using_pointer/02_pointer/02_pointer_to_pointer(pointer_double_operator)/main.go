46ff97965ec23108598b39268b2c3cd59849841b
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-05-06 05:36:24 +0600

    pointer_to_pointer(pointer_double_operator)

--- 02_Second_day_challenge/08_pointers/04_using_pointer/02_pointer/02_pointer_to_pointer(pointer_double_operator)/main.go
+++ 02_Second_day_challenge/08_pointers/04_using_pointer/02_pointer/02_pointer_to_pointer(pointer_double_operator)/main.go
@@ -6,11 +6,12 @@ func main() {
 	var a int = 45
 	var singlePointer *int = &a
 	var pointerTopointer **int = &singlePointer
-	fmt.Println(a)
+	fmt.Println(&a)
 	fmt.Println(singlePointer)
 	fmt.Println(*singlePointer)
 	fmt.Println("-------------------------------------------")
 	fmt.Println(pointerTopointer)
+	fmt.Println(*pointerTopointer)
 	fmt.Println(**pointerTopointer)
 
 }
