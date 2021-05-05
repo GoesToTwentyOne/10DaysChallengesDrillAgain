95c6d6195f40af680507ae2f6481b8b7ab5d50aa
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-05-06 05:03:58 +0600

    using pointer

---
+++ 02_Second_day_challenge/08_pointers/03_using_pointers/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a *int
+	var b int = 45
+	a = &b
+
+	fmt.Println(a)
+	//dereferencing
+	fmt.Println(*a)
+	*a = 69
+	fmt.Println(a)
+	//dereferencing
+	fmt.Println(*a)
+
+}
