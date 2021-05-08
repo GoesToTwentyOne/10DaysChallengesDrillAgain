9247a405e48d728b30df69ea8f16f8a3a887c142
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-05-08 11:33:03 +0600

    parameters

--- 03_Third_day_challenge/14_function/02_parameters_and_arguments/main.go
+++ 03_Third_day_challenge/14_function/02_parameters_and_arguments/main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	goes("Nihad")
+	goes("Rowjatul")
 
 }
 func goes(s string) {
 	fmt.Printf("Hi I am %s \n", s)
 
 }
+
+//when declare a func thats time called parameter
+//when call the func and pass as a arguments with value
